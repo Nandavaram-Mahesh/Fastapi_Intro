a93d139389ab432f61c9e0fc547e7ede1f14ff27
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2026-01-12 05:16:17 +0530

    Added POST /employee route

--- main.py
+++ main.py
@@ -36,3 +36,30 @@ def employee(employee_id:int):
         raise HTTPException(status_code=404 , detail=f"Employee with id {employee_id} not found")
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+@app.post('/employee')
+def create_employee(employee:Employee):
+    try:
+        # Load the employees from json
+        data = load_json()
+        
+        employees = data['employees']
+        
+        # Validate the data
+        employee = Employee.model_validate(employee)
+        
+        #Converting the pydnatic obj to dict  
+        employee = employee.model_dump()
+        
+        # Add the new employee to the employees list 
+        employees.append(employee)
+        
+        # Add the employees to the data dict
+        data["employees"] = employees
+        
+        # Load it to the json file
+        with open('employee.json','w') as file:
+            json.dump(data, file,indent=2)
+        
+        return {"message": "Employee added", "id": employee.get("id")}
+    except Exception as e:
+        raise HTTPException(status_code=500 , detail=str(e))
